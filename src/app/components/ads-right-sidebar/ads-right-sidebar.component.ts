aeffffb9b5f41edae59ac58f5ad1f049ac172abb
nascal3 <nascal3@gmail.com>
2017-09-16 18:58:27 +0300

    show random ads on eatch reload

--- src/app/components/ads-right-sidebar/ads-right-sidebar.component.ts
+++ src/app/components/ads-right-sidebar/ads-right-sidebar.component.ts
@@ -12,6 +12,11 @@ export class AdsRightSidebarComponent implements OnInit {
   ads: any[];
   services: any[];
   sliceCharacters = 102;
+   numberOfAds: number;
+
+  // Number of ads to show slice filter
+  firstSliceVal: number;
+  secondSliceVal: number;
 
   constructor(
     public adsService: AdsServiceService
@@ -26,7 +31,9 @@ export class AdsRightSidebarComponent implements OnInit {
   getAds() {
     this.adsService.fetchAds().subscribe(adverts => {
       this.ads = adverts;
-      // console.log(adverts);
+      this.numberOfAds = adverts.length;
+      this.getRandomInt(0, (this.numberOfAds - 3));
+      // console.log(this.numberOfAds - 3);
     });
   }
 
@@ -35,6 +42,13 @@ export class AdsRightSidebarComponent implements OnInit {
       this.services = services;
       // console.log(services);
     });
+  }
+
+  // Generate random number
+  getRandomInt(min: number, max: number) {
+        this.firstSliceVal =  Math.floor(Math.random() * (max - min + 1)) + min;
+        this.secondSliceVal = this.firstSliceVal + 3;
+        console.log(this.firstSliceVal + '-' + this.secondSliceVal);
   }
 
 }
--- src/app/components/lowerright-sidebar-ads/lowerright-sidebar-ads.component.ts
+++ src/app/components/lowerright-sidebar-ads/lowerright-sidebar-ads.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdsServiceService } from '../../services/ads-service.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-lowerright-sidebar-ads',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LowerrightSidebarAdsComponent implements OnInit {
 
-  constructor() { }
+  ads: any[];
+  sliceTitle = 18;
+  sliceCharacters = 102;
+  numberOfAds: number;
+
+  // Number of ads to show slice filter
+  firstSliceVal: number;
+  secondSliceVal: number;
+
+  constructor(
+    public adsService: AdsServiceService
+  ) {
+    this.getAds();
+  }
 
   ngOnInit() {
   }
+
+  getAds() {
+    this.adsService.fetchAds().subscribe(adverts => {
+      this.ads = adverts;
+      this.numberOfAds = adverts.length;
+      this.getRandomInt(0, (this.numberOfAds - 3));
+      // console.log(this.numberOfAds - 3);
+    });
+  }
+
+   // Generate random number
+  getRandomInt(min: number, max: number) {
+        this.firstSliceVal =  Math.floor(Math.random() * (max - min + 1)) + min;
+        this.secondSliceVal = this.firstSliceVal + 3;
+        console.log(this.firstSliceVal + '-' + this.secondSliceVal);
+  }
+
+
 
 }
