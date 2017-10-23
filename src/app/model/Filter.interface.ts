720fb8a4b0e6a3c71e049d5938d9d399f2428ed7
nascal3 <nascal3@gmail.com>
2017-10-23 16:35:16 +0300

    search filter transmitting search params on change

---
+++ src/app/model/Filter.interface.ts
@@ -0,0 +1,11 @@
+export interface FilterInterface {
+  priceRange?: number[];
+  distanceRange?: number[];
+  starsCount?: number;
+  location?: string;
+  category?: string;
+  offer?: boolean;
+  morning?: string;
+  afternoon?: string;
+  evening?: string;
+}
