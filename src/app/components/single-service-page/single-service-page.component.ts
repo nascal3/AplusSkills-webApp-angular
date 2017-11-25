4105afef2044c792172d905b16cb9c113e7ae1c4
nascal3 <nascal3@gmail.com>
2017-11-25 18:21:06 +0300

    added messaging chat box

--- src/app/components/single-service-page/single-service-page.component.ts
+++ src/app/components/single-service-page/single-service-page.component.ts
@@ -16,6 +16,7 @@ export class SingleServicePageComponent implements OnInit {
   review = true;
   id: string;
   service: any;
+  message: string;
 
   constructor(
     public searchRes: SearchResultService,
